481233eab8a06003de5a1cb3e742abc209631794
Itsisspritzyyy <106392660+Itsisspritzyyy@users.noreply.github.com>
2022-07-10 15:43:01 +0800

    Add files via upload

--- SpritzSploitASSASSIN-free.lua
+++ SpritzSploitASSASSIN-free.lua
@@ -10,10 +10,16 @@ local VisualTab = SpritzSploit:Tab("Visuals")
 local LocalTab = SpritzSploit:Tab("LocalPlayer")
 local DescTab = SpritzSploit:Tab("Description")
 
+local CoreGui = game.CoreGui
+local ContentProvider = game.ContentProvider
+local RobloxGuis = {"RobloxGui", "TeleportGui", "RobloxPromptGui", "RobloxLoadingGui", "PlayerList", "RobloxNetworkPauseNotification", "PurchasePrompt", "HeadsetDisconnectedDialog", "ThemeProvider", "DevConsoleMaster"}
+
+
 local lp = game.Players.LocalPlayer
 local Player = game:GetService("Players").LocalPlayer
 local Camera = game:GetService("Workspace").CurrentCamera
 local Mouse = game:GetService("Players").LocalPlayer:GetMouse()
+getgenv().noclippath = false
 
 local ttable = {"BALLSBALLSBALLSBALLSBALLSBALLSBALLSBALLSBALLSBALLSBALLSBALLSBALLSBALLSBALLSBALLSBALLSBALLSBALLSBALLSBALLSBALLSBALLSBALLSBALLSBALLSBALLSBALLSBALLSBALLSBALLSBALLSBALLSBALLSBALLSBALLSBALLSBALLSBALLSBALLSBALLSBALLSBALLSBALLSBALLSBALLSBALLSBALLSBALLSBALLSBALLSBALLSBALLSBALLSBALLSBALLSBALLSBALLSBALLSBALLSBALLSBALLSBALLSBALLSBALLSBALLSBALLSBALLSBALLSBALLS", "COCKCOCKCOCKCOCKCOCKCOCKCOCKCOCKCOCKCOCKCOCKCOCKCOCKCOCKCOCKCOCKCOCKCOCKCOCKCOCKCOCKCOCKCOCKCOCKCOCKCOCKCOCKCOCKCOCK"}
 local ESP = loadstring(game:HttpGet("https://kiriot22.com/releases/ESP.lua"))()
@@ -25,7 +31,6 @@ ESP.Names = false;
 ESP.Thickness = 2;
 ESP.Color = Color3.fromRGB(255, 255, 255)
 
-
 FOV_Circle = Drawing.new("Circle")
 FOV_Circle.Color = Color3.fromRGB(255,255,255)
 FOV_Circle.Thickness = 2.5
@@ -34,6 +39,15 @@ FOV_Circle.Radius = 200
 FOV_Circle.Visible = false
 FOV_Circle.Filled = false
 
+function GetTime(Distance, Speed)
+    local Time = Distance / Speed
+    return Time
+end
+
+function spritzTween(targetpart,spritzspeed)
+    local Speed = spritzspeed  local Distance = (game.Players.LocalPlayer.Character.HumanoidRootPart.Position - targetpart.Position).magnitude local Time = GetTime(Distance, Speed) local TweenService = game:GetService("TweenService") local spritzttable = TweenInfo.new( Time, Enum.EasingStyle.Linear, Enum.EasingDirection.Out,  0, false, 0 ) local Tween = TweenService:Create(game.Players.LocalPlayer.Character.HumanoidRootPart,spritzttable,{CFrame = targetpart.CFrame}) Tween:Play()
+end
+
 _G.Toggle = false
 CombatTab:Toggle("Silent Aim",false, function(t)
     _G.Toggle = t
@@ -41,6 +55,15 @@ end)
 
 CombatTab:Toggle("Show FOV",false, function(t)
     FOV_Circle.Visible = t
+end)
+
+CombatTab:Button("Target PathFinder", function()
+    local tptotarget = true
+    while tptotarget == true do
+        spritzTween(game.Workspace[game.Players.LocalPlayer.PlayerGui.ScreenGui:WaitForChild("UI"):WaitForChild("Target"):WaitForChild("TargetText").Text].HumanoidRootPart,30)
+        wait(0.2)
+    end
+    noclippath = true
 end)
 
 getgenv().ifuckinghatelife = false
@@ -231,4 +254,109 @@ game.Workspace.ChildAdded:Connect(function(child)
             Color = Color3.fromRGB(255,255,255)
         })
     end
+end)
+
+if noclippath == true then 
+    for _,v in pairs(char:children()) do 
+        pcall(function() if v.className == "Part" then v.CanCollide = false elseif v.ClassName == "Model" then
+            v.Head.CanCollide = false end end) 
+    end 
+end 
+
+local function FilterTable(tbl)
+    local context = syn_context_get()
+    syn_context_set(7)
+    local new = {}
+    for i,v in ipairs(tbl) do --roblox iterates the array part
+        if typeof(v) ~= "Instance" then
+            table.insert(new, v)
+        else
+            if v == CoreGui or v == game then
+                --insert only the default roblox guis
+                for i,v in pairs(RobloxGuis) do
+                    local gui = CoreGui:FindFirstChild(v)
+                    if gui then
+                        table.insert(new, gui)
+                    end
+                end
+
+                if v == game then
+                    for i,v in pairs(game:GetChildren()) do
+                        if v ~= CoreGui then
+                            table.insert(new, v)
+                        end
+                    end
+                end
+            else
+                if not CoreGui:IsAncestorOf(v) then
+                    table.insert(new, v)
+                else
+                    --don't insert it if it's a descendant of a different gui than default roblox guis
+                    for j,k in pairs(RobloxGuis) do
+                        local gui = CoreGui:FindFirstChild(k)
+                        if gui then
+                            if v == gui or gui:IsAncestorOf(v) then
+                                table.insert(new, v)
+                                break
+                            end
+                        end
+                    end
+                end
+            end
+        end
+    end
+    syn_context_set(context)
+    return new
+end
+
+local old
+old = hookfunc(ContentProvider.PreloadAsync, function(self, tbl, cb)
+    if self ~= ContentProvider or type(tbl) ~= "table" or type(cb) ~= "function" then --note: callback can be nil but in that case it's useless anyways
+        return old(self, tbl, cb)
+    end
+
+    --check for any errors that I might've missed (such as table being {[2] = "something"} which causes "Unable to cast to Array")
+    local err
+    task.spawn(function() --TIL pcalling a C yield function inside a C yield function is a bad idea ("cannot resume non-suspended coroutine")
+        local s,e = pcall(old, self, tbl)
+        if not s and e then
+            err = e
+        end
+    end)
+   
+    if err then
+        return old(self, tbl) --don't pass the callback, just in case
+    end
+
+    tbl = FilterTable(tbl)
+    return old(self, tbl, cb)
+end)
+
+local old
+old = hookmetamethod(game, "__namecall", function(self, ...)
+    local method = getnamecallmethod()
+    if self == ContentProvider and (method == "PreloadAsync" or method == "preloadAsync") then
+        local args = {...}
+        if type(args[1]) ~= "table" or type(args[2]) ~= "function" then
+            return old(self, ...)
+        end
+
+        local err
+        task.spawn(function()
+            setnamecallmethod(method) --different thread, different namecall method
+            local s,e = pcall(old, self, args[1])
+            if not s and e then
+                err = e
+            end
+        end)
+
+        if err then
+            return old(self, args[1])
+        end
+
+        args[1] = FilterTable(args[1])
+        setnamecallmethod(method)
+        return old(self, args[1], args[2])
+    end
+    return old(self, ...)
 end)
